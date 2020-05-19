b7d45d2d59a3226dc7673a897244bad372beebab
jorge eduardo valencia loyola <jevl290397@gmail.com>
2020-05-18 19:17:43 -0500

    Estructura base del proyecto en angular

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+    ],
+    exports: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+    ]
+})
+
+export class SharedModule { }
